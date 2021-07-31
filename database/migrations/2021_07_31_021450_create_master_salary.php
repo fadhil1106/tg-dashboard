44a88a2e57df702dcc35b9ed5eca83e9bb6d8557
Fuad Fadhil Azzar <30482903+fadhil1106@users.noreply.github.com>
2021-07-31 10:31:08 +0800

    create Module Fuel & Module Salary

--- database/migrations/2021_07_31_021450_create_master_salary.php
+++ database/migrations/2021_07_31_021450_create_master_salary.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMasterFuel extends Migration
+class CreateMasterSalary extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateMasterFuel extends Migration
      */
     public function up()
     {
-        Schema::create('master_fuel', function (Blueprint $table) {
+        Schema::create('master_salary', function (Blueprint $table) {
             $table->id();
-            $table->date('fuel_date');
-            $table->bigInteger('fuel_ti');
-            $table->bigInteger('fuel_tm');
-            $table->string('fuel_comment');
+            $table->date('salary_date');
+            $table->bigInteger('salary_ti');
+            $table->bigInteger('salary_tm');
+            $table->string('salary_comment');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateMasterFuel extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('master_fuel');
+        Schema::dropIfExists('master_salary');
     }
 }
